1bba53bcd93b7c1a00932ac22c76007c1a2734c5
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-09-17 13:21:59 +0300

    1. Updated Constraints 2. Rounded button corners 3. Round traffic light signals

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+        switchButton.layer.cornerRadius = 30
+        
     }
 
-
+    @IBAction func pressButton() {
+    }
+    
 }
 
